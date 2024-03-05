865602bbb0da16c5cfcf110d810cbfc499e9a453
Elena <femtochka@yahoo.com>
2024-03-05 18:26:40 +0300

    hw7.3

--- app/src/main/java/ru/elenandreyuk/quizzbygb/MainActivity.kt
+++ app/src/main/java/ru/elenandreyuk/quizzbygb/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import ru.elenandreyuk.quizzbygb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_navigation) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
